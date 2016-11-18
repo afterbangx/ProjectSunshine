5bb4ec20071c22527d20f6500a6f33e971ccbf0f
Andy Milbeck <amilbeck@appriver.corp>
2016-11-18 14:29:01 -0600

    adding some fake data

--- app/src/main/java/app/com/example/android/projectsunshine/PlaceholderFragment.java
+++ app/src/main/java/app/com/example/android/projectsunshine/PlaceholderFragment.java
@@ -6,11 +6,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PlaceholderFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_placeholder, container, false);
     }
