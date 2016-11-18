cd90f6686ed263eb8ad0731dfa196ca45eb79f95
Andy Milbeck <amilbeck@appriver.corp>
2016-11-18 14:57:28 -0600

    adding adapter to fragment

--- app/src/main/java/app/com/example/android/projectsunshine/PlaceholderFragment.java
+++ app/src/main/java/app/com/example/android/projectsunshine/PlaceholderFragment.java
@@ -5,12 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PlaceholderFragment extends Fragment {
+    ArrayAdapter<String> forecastAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,6 +28,12 @@ public class PlaceholderFragment extends Fragment {
                 "Sun 6/29 - Sunny - 20/7"
         };
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        forecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.listitem_forecast,
+                R.id.listitem_forecast_textview,
+                weekForecast);
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_placeholder, container, false);
