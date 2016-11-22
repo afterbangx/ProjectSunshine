52327c2ae5d237641637419c5b97c6b6b0e5c8e9
Andy Milbeck <amilbeck@appriver.corp>
2016-11-22 10:12:31 -0600

    renamed fragment and moved network call to task class

--- app/src/main/java/app/com/example/android/projectsunshine/FetchWeatherTask.java
+++ app/src/main/java/app/com/example/android/projectsunshine/FetchWeatherTask.java
@@ -1,13 +1,7 @@
 package app.com.example.android.projectsunshine;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,41 +9,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-public class PlaceholderFragment extends Fragment {
-    ArrayAdapter<String> forecastAdapter;
+/**
+ * Created by amilbeck on 11/22/2016.
+ */
+
+public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+    private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_placeholder, container, false);
-
-        //creating some fake data
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
-        forecastAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.listitem_forecast,
-                R.id.listitem_forecast_textview,
-                weekForecast);
-
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-        listView.setAdapter(forecastAdapter);
-
+    protected Void doInBackground(Void... params) {
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
 
@@ -109,7 +78,6 @@ public class PlaceholderFragment extends Fragment {
                 }
             }
         }
-
-        return rootView;
+        return null;
     }
 }
---
+++ app/src/main/java/app/com/example/android/projectsunshine/ForecastFragment.java
@@ -0,0 +1,57 @@
+package app.com.example.android.projectsunshine;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ForecastFragment extends Fragment {
+    ArrayAdapter<String> forecastAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        //creating some fake data
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        forecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.listitem_forecast,
+                R.id.listitem_forecast_textview,
+                weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
+
+
+
+        return rootView;
+    }
+}
