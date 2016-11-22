bdf69fe6a2eca2765468483e367d0c54df96657f
Andy Milbeck <amilbeck@appriver.corp>
2016-11-22 11:17:40 -0600

    adding menu items

--- app/src/main/java/app/com/example/android/projectsunshine/ForecastFragment.java
+++ app/src/main/java/app/com/example/android/projectsunshine/ForecastFragment.java
@@ -3,6 +3,9 @@ package app.com.example.android.projectsunshine;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -15,12 +18,41 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
     ArrayAdapter<String> forecastAdapter;
 
+    public ForecastFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //this is needed in order to handle menu events
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.refresh_forecast, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if(id == R.id.action_refresh) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         //creating some fake data
         String[] data = {
